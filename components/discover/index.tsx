f75d2a4bd8084592f86844fa952f95c47efdc19c
Jeremiah Ibiang Eni <jerryeni3@gmail.com>
2024-12-21 20:45:40 +0100

    update

--- components/discover/index.tsx
+++ components/discover/index.tsx
@@ -6,8 +6,8 @@ export function Discover() {
   return (
     <section className="relative mt-20 max-w-5xl mx-auto">
       {/* Yellow border at the top */}
-      <div className=" -mt-20 rounded-[48px] bg-yellow-400">
-        <div className="container mx-auto px-4 text-center py-32">
+      <div className=" -mt-20 px-2">
+        <div className="container mx-auto px-4 text-center py-32 rounded-[32px] bg-black border-t-4 border-yellow-400">
           <h2 className="text-5xl font-bold text-white mb-6">
             XXXXXXX
           </h2>
@@ -16,21 +16,6 @@ export function Discover() {
           </p>
           <Button 
             className="bg-[#FFE872] text-black hover:bg-[#f5df5f] rounded-full px-8 py-6 font-medium"
-          >
-            Discover Universe
-          </Button>
-        </div>
-      </div>
-      <div className="discover-section top-1 z-50 w-full bg-yellow-400 absolute">
-        <div className="container mx-auto px-4 text-center py-32">
-          <h2 className="text-5xl font-bold text-white mb-6">
-            XXXXXXX
-          </h2>
-          <p className="text-gray-400 mb-8">
-            XXXXXXXXXXXXXXXXXXXXXXXX
-          </p>
-          <Button 
-            className="bg-[#FFE872] text-black hover:bg-[#f5df5f] rounded-xl px-8 py-6 font-medium"
           >
             Discover Universe
           </Button>
